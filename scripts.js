9f7532305c46d7d4e02055d9b426000270bfbcfd
Erica Naglik <erica2605@live.com>
2018-12-05 22:45:23 -0800

    javascript file added

---
+++ scripts.js
@@ -0,0 +1,58 @@
+ //declare default variables
+ let cookieCount = 0;
+ let clickPower = 1;
+ let clickPowerPriceAmount = 50;
+ let clickPowerLevelNumber = 1;
+
+     //declare DOM variables 
+     let cookieCounter = document.getElementById('cookie-counter');
+     let cookieClicker = document.getElementById('cookie-clicker');
+     let buyClickPower = document.getElementById('buy-click-power');
+     let clickPowerPrice = document.getElementById('click-power-price');
+     let clickPowerLevel = document.getElementById('click-power-level');
+     let clickPowerMultiple = document.getElementById('click-power-multiple');
+
+     cookieCounter.innerHTML = cookieCount;
+ 
+ //everytime a user clicks the button, their cookies are increased by the value of their clickPower.
+ cookieClicker.addEventListener("click", function() {
+     cookieCount = cookieCount + clickPower;
+     refreshCookieCount()
+ })
+
+ //Buy click power
+ buyClickPower.addEventListener("click", function() {
+     if (cookieCount >= clickPowerPriceAmount) {
+         //subtract cookies from the price of the item
+         cookieCount -= clickPowerPriceAmount;
+
+         //update cookie counter
+         refreshCookieCount()
+
+         //Upgrade power level
+         clickPowerLevelNumber += 1;
+
+         //Update click price 
+         clickPowerPriceAmount = Math.floor(clickPowerPriceAmount * 1.33);
+
+         //Update Click Power 
+         clickPower += 1;
+
+         //refresh shop item
+         refreshPowerClick();
+
+         console.log("Item successfully Bought");
+     } else {
+         console.log("You don't have enough cookies!");
+     }
+ })
+
+ let refreshCookieCount = function() {
+     cookieCounter.innerHTML = cookieCount;
+ }
+
+ let refreshPowerClick = function() {
+     clickPowerLevel.innerHTML = clickPowerLevelNumber;
+     clickPowerPrice.innerHTML = clickPowerPriceAmount;
+     clickPowerMultiple.innerHTML = clickPower;
+ }
